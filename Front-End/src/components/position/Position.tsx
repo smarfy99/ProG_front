28010e275708fa38dec23be677cdc728b917190e
백진규 <qorwlsrb01@naver.com>
2024-02-02 09:05:00 +0900

    [FE] Feat : Add 프로젝트 설정 페이지

--- Front-End/src/components/position/Position.tsx
+++ Front-End/src/components/position/Position.tsx
@@ -1,6 +1,24 @@
 import React, { useState, ChangeEvent } from 'react';
 
+
 interface PositionState {
+<<<<<<< HEAD
+  positionName: string[];
+  positionNumber: number[];
+  positionCurrent: number[];
+}
+
+interface PositionItem {
+  jobCode: string;
+  total: number;
+  current: number;
+}
+
+const positionList = ["포지션을 선택해 주세요", "프론트엔드", "백엔드", "기획", "디자인", "UI/UX"];
+
+export const position = {
+  totalList: [{ jobCode: '', total: 1, current: 0 }] as PositionItem[],
+=======
 	positionName: string[];
 	positionNumber: number[];
 	positionCurrent: number[];
@@ -16,10 +34,210 @@ const positionList = ['포지션을 선택해 주세요', '프론트엔드', '�
 
 export const position = {
 	totalList: [{ jobCode: '', total: 1, current: 0 }] as PositionItem[],
+>>>>>>> e3796a8914ef5ddb56ddbc25140348bf1ef8a6b8
 };
 
 
 const Position: React.FC = () => {
+<<<<<<< HEAD
+  const [state, setState] = useState<PositionState>({
+    positionName: [''],
+    positionNumber: [1],
+    positionCurrent: [0],
+  });
+
+  const handlePositionNameChange = (index: number, value: string) => {
+    const updatedPositionName = [...state.positionName];
+    updatedPositionName[index] = value;
+  
+    const updatedPositionCurrent = Array(state.positionCurrent.length).fill(0);
+    updatedPositionCurrent[index] = state.positionCurrent[index];
+  
+    setState((prev) => ({
+      ...prev,
+      positionName: updatedPositionName,
+      positionCurrent: updatedPositionCurrent,
+    }));
+  
+    if (position.totalList[index]) {
+      position.totalList[index].jobCode = value;
+    }
+  };
+  
+  const handleParticipateButtonClick = (index: number) => {
+    const updatedPositionCurrent = Array(state.positionCurrent.length).fill(0);
+    updatedPositionCurrent[index] = 1;
+    console.log(updatedPositionCurrent);
+    console.log(state.positionCurrent);
+    
+    setState((prev) => ({
+      ...prev,
+      positionCurrent: updatedPositionCurrent,
+    }));
+    for(let i = 0; i < state.positionCurrent.length; i++){
+      position.totalList[i].current = 0;
+      console.log(i);
+      if(i>100){
+        break;
+      }
+    }
+    position.totalList[index].current = 1;
+  };
+  const handleAddPositionName = () => {
+    setState((prev) => {
+      const newPositionName = [...prev.positionName, ''];
+      const newPositionNumber = [...prev.positionNumber, 1];
+      const newPositionCurrent = [...prev.positionCurrent, 0];
+
+      return {
+        positionName: newPositionName,
+        positionNumber: newPositionNumber,
+        positionCurrent: newPositionCurrent,
+      };
+    });
+    position.totalList.push({ jobCode: '', total: 1, current: 0 });
+  };
+
+
+
+
+  const handleRemovePositionName = (index: number) => {
+    const updatedPositionName = [...state.positionName];
+    const updatedPositionNumber = [...state.positionNumber];
+    const updatedPositionCurrent = [...state.positionCurrent];
+    updatedPositionName.splice(index, 1);
+    updatedPositionNumber.splice(index, 1);
+    updatedPositionCurrent.splice(index, 1);
+    setState({
+      positionName: updatedPositionName,
+      positionNumber: updatedPositionNumber,
+      positionCurrent: updatedPositionCurrent,
+    });
+    position.totalList.splice(index, 1);
+  };
+
+  const handlePositionNumberChange = (index: number, value: number) => {
+    // Ensure the index is valid
+    if (index < 0 || index >= state.positionNumber.length) {
+      return;
+    }
+
+    // Update the state with the new position number
+    setState((prev) => {
+      const updatedPositionNumber = [...prev.positionNumber];
+      updatedPositionNumber[index] = Math.max(1, value);
+
+      // Update the totalList array
+      const updatedTotalList = [...position.totalList];
+      updatedTotalList[index] = {
+        ...updatedTotalList[index],
+        total: updatedPositionNumber[index],
+      };
+
+      return {
+        ...prev,
+        positionNumber: updatedPositionNumber,
+      };
+    });
+  };
+
+
+  const handleIncrementPositionNumber = (index: number) => {
+    const updatedPositionNumber = [...state.positionNumber];
+    updatedPositionNumber[index] += 1;
+    setState((prev) => ({
+      ...prev,
+      positionNumber: updatedPositionNumber,
+    }));
+    position.totalList[index].total = updatedPositionNumber[index];
+  };
+
+  const handleDecrementPositionNumber = (index: number) => {
+    const updatedPositionNumber = [...state.positionNumber];
+    updatedPositionNumber[index] = Math.max(1, updatedPositionNumber[index] - 1);
+    setState((prev) => ({
+      ...prev,
+      positionNumber: updatedPositionNumber,
+    }));
+    position.totalList[index].total = updatedPositionNumber[index];
+  };
+
+  return (
+    <div>
+      <div className='my-3'>
+        <label htmlFor='PositionName' className='font-bold text-lg my-3'>
+          포지션
+        </label>
+        <div>
+          {state.positionName.map((positionName, index) => (
+            <div key={index} className='flex items-center mb-2'>
+              <button
+                onClick={() => handleRemovePositionName(index)}
+                className='mr-2 p-2 pr-3 bg-red-500 text-white'
+              >
+                -
+              </button>
+              <select
+                id={`PositionName-${index}`}
+                name={`PositionName-${index}`}
+                className='w-1/3 h-10'
+                value={positionName}
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                  handlePositionNameChange(index, e.target.value);
+                }}
+              >
+                {positionList.map((option, optionIndex) => (
+                  <option key={optionIndex} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+
+              <div className='flex items-center'>
+                <button
+                  onClick={() => handleDecrementPositionNumber(index)}
+                  className={`p-2 bg-blue-500 text-white ${state.positionNumber[index] <= 1 ? 'bg-gray-300 cursor-not-allowed' : ''
+                    }`}
+                  disabled={state.positionNumber[index] <= 1}
+                >
+                  -
+                </button>
+                <input
+                  type='text'
+                  id={`PositionNumber-${index}`}
+                  name={`PositionNumber-${index}`}
+                  className='w-1/3 h-10'
+                  placeholder='인원수'
+                  value={state.positionNumber[index] || 1}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handlePositionNumberChange(index, +e.target.value)
+                  }
+                />
+                <button
+                  onClick={() => handleIncrementPositionNumber(index)}
+                  className={`p-2 bg-blue-500 text-white ${state.positionNumber[index] >= 10 ? 'bg-gray-300 cursor-not-allowed' : ''
+                    }`}
+                  disabled={state.positionNumber[index] >= 10}
+                >
+                  +
+                </button>
+                <button
+                  className={`flex items-center ml-2 border-main-color border-2 p-2 ${state.positionCurrent[index] === 1 ? 'bg-main-color text-white' : ''}`}
+                  onClick={() => handleParticipateButtonClick(index)}
+                >
+                  참여
+                </button>
+              </div>
+            </div>
+          ))}
+          <button onClick={handleAddPositionName} className='p-2 bg-green-500 text-white'>
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+=======
 	const [state, setState] = useState<PositionState>({
 		positionName: [''],
 		positionNumber: [1],
@@ -211,6 +429,7 @@ const Position: React.FC = () => {
 			</div>
 		</div>
 	);
+>>>>>>> e3796a8914ef5ddb56ddbc25140348bf1ef8a6b8
 };
 
 export default Position;
--- Front-End/src/pages/home/HomePage.tsx
+++ Front-End/src/pages/home/HomePage.tsx
@@ -1,5 +1,33 @@
+import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+	const navigate = useNavigate();
+
+	const moveRecruit = () => {
+		navigate('../recruit/');
+	};
+
+	const moveLogin = () => {
+		navigate('../login/');
+	};
+
+	return (
+		<div>
+			<div className='flex flex-col items-center justify-center'>
+				<img src={logo} alt='React logo' className='w-4/12 h-auto' />
+				<div className='font-bold text-4xl'>Programmer가 되고 싶은 ProG들</div>
+				<div className='grid items-center justify-center text-center'>
+					<span className='bg-main-color p-5 m-3 text-xl text-white cursor-pointer' onClick={moveRecruit}>
+						프로젝트 찾아보기
+					</span>
+					<span className='bg-yellow-300 p-5 mx-3 text-xl cursor-pointer' onClick={moveLogin}>
+						로그인으로 시작하기
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
