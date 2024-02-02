386ee8c6e4b6435df44db3e5df22ee759b2d1427
smarfy99 <jjwzzy4643@gmail.com>
2024-02-02 14:25:12 +0900

    [FE] Feat : 로그인 처리 성공

--- Front-End/src/apis/lib/axios.ts
+++ Front-End/src/apis/lib/axios.ts
@@ -3,6 +3,7 @@ import { useAuthStore } from "../../stores/useAuthStore";
 
 export const axiosInstance = axios.create({
   baseURL: "http://i10a210.p.ssafy.io:8080",
+  // baseURL: "http://localhost:8080",
   withCredentials: true, //쿠키 포함
 });
 
@@ -11,7 +12,9 @@ axiosInstance.interceptors.request.use((config) => {
   const accessToken = useAuthStore.getState().accessToken;
   if (accessToken && config.headers) {
     config.headers["Authorization"] = `Bearer ${accessToken}`;
-    console.log(`Setting Authorization header: ${config.headers["Authorization"]}`); // accessToken 로그 출력
+    console.log(
+      `Setting Authorization header: ${config.headers["Authorization"]}`
+    ); // accessToken 로그 출력
   }
   return config;
 });
