8520c1284fe2ece8b8a542e3b1619674150eaacd
smarfy99 <jjwzzy4643@gmail.com>
2024-02-15 20:56:52 +0900

    [FE] Chore : vite.config proxy 주석처리

--- Front-End/vite.config.ts
+++ Front-End/vite.config.ts
@@ -4,28 +4,28 @@ import path from 'path';
 
 export default defineConfig({
 	base: './',
-	server: {
-		port: 3000,
-		host: '0.0.0.0',
-		proxy: {
-			'/members/reissue-token': {
-				target: 'http://i10a210.p.ssafy.io:8080/',
-				changeOrigin: true,
-			},
-			'/members/logout': {
-				target: 'http://i10a210.p.ssafy.io:8080/',
-				changeOrigin: true,
-			},
-			'/members/login': {
-				target: 'http://i10a210.p.ssafy.io:8080/',
-				changeOrigin: true,
-			},
-			'/members/login/oauth2/github': {
-				target: 'http://i10a210.p.ssafy.io:8080/',
-				changeOrigin: true,
-			},
-		},
-	},
+	// server: {
+	// 	port: 3000,
+	// 	host: '0.0.0.0',
+	// 	proxy: {
+	// 		'/members/reissue-token': {
+	// 			target: 'http://i10a210.p.ssafy.io:8080/',
+	// 			changeOrigin: true,
+	// 		},
+	// 		'/members/logout': {
+	// 			target: 'http://i10a210.p.ssafy.io:8080/',
+	// 			changeOrigin: true,
+	// 		},
+	// 		'/members/login': {
+	// 			target: 'http://i10a210.p.ssafy.io:8080/',
+	// 			changeOrigin: true,
+	// 		},
+	// 		'/members/login/oauth2/github': {
+	// 			target: 'http://i10a210.p.ssafy.io:8080/',
+	// 			changeOrigin: true,
+	// 		},
+	// 	},
+	// },
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
