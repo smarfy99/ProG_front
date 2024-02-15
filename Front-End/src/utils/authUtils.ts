36ce999b74a9110f3d7ce3c8afd115cd741a2544
smarfy99 <jjwzzy4643@gmail.com>
2024-02-16 08:51:03 +0900

    [FE] Feat : proxy 분기처리 및 build, 배포

--- Front-End/src/utils/authUtils.ts
+++ Front-End/src/utils/authUtils.ts
@@ -1,11 +1,11 @@
-import { axiosInstance } from '../apis/lib/axios';
-// import { proxyAxiosInstance } from '../apis/lib/proxyAxios';
+// import { axiosInstance } from '../apis/lib/axios';
+import { proxyAxiosInstance } from '../apis/lib/proxyAxios';
 import { useAuthStore } from '../stores/useAuthStore';
 
 //accessToken 재발급 함수
 export const reissueToken = async () => {
 	try {
-		const response = await axiosInstance.get('/members/reissue-token');
+		const response = await proxyAxiosInstance.get('/members/reissue-token');
 		const newAccessToken = response.headers['accesstoken'];
 		if (newAccessToken) {
 			useAuthStore.getState().setAccessToken(newAccessToken);
--- Front-End/src/utils/logout.ts
+++ Front-End/src/utils/logout.ts
@@ -1,12 +1,12 @@
-import { axiosInstance } from '../apis/lib/axios';
-// import { proxyAxiosInstance } from '../apis/lib/proxyAxios';
+// import { axiosInstance } from '../apis/lib/axios';
+import { proxyAxiosInstance } from '../apis/lib/proxyAxios';
 import { NavigateFunction } from 'react-router-dom';
 import { useAuthStore } from '../stores/useAuthStore';
 import { useUserStore } from '../stores/useUserStore';
 
 export const logout = async (navigate: NavigateFunction) => {
 	try {
-		await axiosInstance.post('/members/logout');
+		await proxyAxiosInstance.post('/members/logout');
 	} catch (error) {
 		console.error('Failed to logout', error);
 	}
