7f39da6f085b25be8c133d6682fc369dacf5151f
smarfy99 <jjwzzy4643@gmail.com>
2024-02-15 13:46:56 +0900

    F/test -> f/dev

--- Front-End/src/components/mypage/MyProfile.tsx
+++ Front-End/src/components/mypage/MyProfile.tsx
@@ -1,41 +1,41 @@
-import {useState} from "react";
-import MyProject from "./MyProject.tsx";
-import MyProfileDetail from "./MyProfileDetail.tsx";
+import { useState } from 'react';
+import MyProject from './MyProject.tsx';
+import MyProfileDetail from './MyProfileDetail.tsx';
 
-type ComponentType = "MyProfileDetail" | "MyGit" | "MyProject";
+type ComponentType = 'MyProfileDetail' | 'MyGit' | 'MyProject';
 
 const MyProfile = () => {
-  const [selectedComponentType, setSelectedComponentType] = useState("MyProfileDetail");
+	const [selectedComponentType, setSelectedComponentType] = useState('MyProfileDetail');
 
-  const handleProjectTypeChange = (type: ComponentType) => {
-    setSelectedComponentType(type);
-  };
+	const handleProjectTypeChange = (type: ComponentType) => {
+		setSelectedComponentType(type);
+	};
 
-  return (
-      <div>
-          <div className="flex justify-center">
-              <div
-                  className={`mx-16 text-2xl cursor-pointer ${selectedComponentType === "MyProfileDetail"
-                      ? "font-semibold text-main-color"
-                      : ""}`}
-                  onClick={() => handleProjectTypeChange("MyProfileDetail")}
-              >
-                  상세정보
-              </div>
-              <div
-                  className={`mx-16 text-2xl cursor-pointer ${selectedComponentType === "MyProject"
-                      ? "font-semibold text-main-color"
-                      : ""}`}
-                  onClick={() => handleProjectTypeChange("MyProject")}
-              >
-                  나의 프로젝트
-              </div>
-          </div>
+	return (
+		<div>
+			<div className='flex justify-center'>
+				<div
+					className={`mx-16 text-2xl cursor-pointer ${
+						selectedComponentType === 'MyProfileDetail' ? 'font-semibold text-main-color' : ''
+					}`}
+					onClick={() => handleProjectTypeChange('MyProfileDetail')}
+				>
+					상세정보
+				</div>
+				<div
+					className={`mx-16 text-2xl cursor-pointer ${
+						selectedComponentType === 'MyProject' ? 'font-semibold text-main-color' : ''
+					}`}
+					onClick={() => handleProjectTypeChange('MyProject')}
+				>
+					나의 프로젝트
+				</div>
+			</div>
 
-          {selectedComponentType === "MyProfileDetail" && <MyProfileDetail/>}
-          {selectedComponentType === "MyProject" && <MyProject/>}
-      </div>
-  );
+			{selectedComponentType === 'MyProfileDetail' && <MyProfileDetail />}
+			{selectedComponentType === 'MyProject' && <MyProject />}
+		</div>
+	);
 };
 
 export default MyProfile;
