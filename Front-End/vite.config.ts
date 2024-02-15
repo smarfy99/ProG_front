5f7b8fd1cc61a9340a03c59d1fe6ad27067246cb
전승열 <wjs6265@gmail.com>
2024-02-16 00:52:31 +0900

    Update vite.config.ts

--- Front-End/vite.config.ts
+++ Front-End/vite.config.ts
@@ -4,13 +4,13 @@ import path from 'path';
 
 export default defineConfig({
 	base: './',
-	// server: {
-	// 	port: 3000,
-	// 	host: '0.0.0.0',
+	 server: {
+	 	port: 3000,
+	 	host: '0.0.0.0',
 	// 	proxy: {
 	// 		'/members/reissue-token': {
 	// 			target: 'http://i10a210.p.ssafy.io:8080/',
-	// 			changeOrigin: true,
+	//			changeOrigin: true,
 	// 		},
 	// 		'/members/logout': {
 	// 			target: 'http://i10a210.p.ssafy.io:8080/',
@@ -25,7 +25,7 @@ export default defineConfig({
 	// 			changeOrigin: true,
 	// 		},
 	// 	},
-	// },
+	 },
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
